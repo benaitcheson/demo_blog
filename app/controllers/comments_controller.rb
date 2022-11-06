aedf67a3975c8f0affcaa9688a6b7f6e64df595a
Ben Aitcheson <benaitcheson@hotmail.com>
2022-11-07 08:13:53 +1000

    feat: get fly.io set up and fix comments controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,10 +3,13 @@ class CommentsController < ApplicationController
   def create
     @post = Post.find(params[:post_id])
     @comment = @post.comments.create(params[:comment]).permit(:name, :comment)
-    redirect_to posts_path(@post)
+    redirect_to post_path(@post)
   end
 
   def destroy
-
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.find(params[:id])
+    @comment.destroy
+    redirect_to post_path(@post)
   end
 end
