84dda6f68bc428cad4872d3749fff06ed641b436
Ben Aitcheson <benaitcheson@hotmail.com>
2022-11-06 16:09:36 +1000

    feat: add column to comments for post_id

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,10 @@
 class CommentsController < ApplicationController
+
+  def create
+    @post = Post.find(params[:post_id])
+  end
+
+  def destroy
+
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_06_060320) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_06_060741) do
   create_table "comments", force: :cascade do |t|
     t.string "name"
     t.text "comment"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "post_id"
   end
 
   create_table "posts", force: :cascade do |t|
