db3956c9f4519ea729b90261badffb2790f99301
Ben Aitcheson <benaitcheson@hotmail.com>
2022-11-06 14:11:45 +1000

    feat: create action updated

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,5 +8,18 @@ class PostsController < ApplicationController
   end
 
   def create
+    @post = Post.new(post_params)
+
+    if @post.save
+      redirect_to @post
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :content)
   end
 end
